360100374283d6403d9702ba803d2ad889c4b54e
Alvaro <alvaro.iglesiasarias@gmail.com>
2020-12-28 12:15:55 +0100

    BreakOut Game: todo add lateral collision physics.

--- Project5/classes.js
+++ Project5/classes.js
@@ -1,3 +1,4 @@
+//@ts-check
 'use strict';
 
 export class rectangle {
@@ -7,12 +8,12 @@ export class rectangle {
         this.y = initPoint[1];
 
         this.width = size[0];
-        this.heigth = size[1];
+        this.height = size[1];
 
     }
     
     getCenter(){
-        return([(this.width + this.x)/2, (this.heigth+this.y)/2]);
+        return([(this.width + this.x)/2, (this.height+this.y)/2]);
     }
 
     setCenter(point){
@@ -20,11 +21,11 @@ export class rectangle {
         let cy = point[1];
 
         this.x = cx - this.width/2;
-        this.y = cy - this.heigth/2;
+        this.y = cy - this.height/2;
     }
 
     width = 0;
-    heigth = 0;
+    height = 0;
     x = 0;
     y = 0;
 }
@@ -37,10 +38,12 @@ export class screen {
     constructor(canvas) {
         this.width = canvas.width;
         this.height = canvas.height;
+        this.ctx = canvas.getContext('2d');
     }
 
     width;
     height;
+    ctx;
 }
 
 export class ball {
@@ -48,6 +51,34 @@ export class ball {
         this.x = Point[0];
         this.y = Point[1];
         this.radius = 10;
-        this.speed = [10, -10];
+        this.speed = [5, -5];
     }
+
+    setPosition(Point) {
+        this.x = Point[0];
+        this.y = Point[1];
+    }
+}
+
+export class bricks {
+
+    constructor(screen, nRows, nCols) {
+
+        let margin_X = 10; //px
+        let margin_Y = 10; //px
+        let brick_width = ( (screen.width - margin_X) / nCols) - margin_X;
+        let brick_height = ((0.6*screen.height) / nRows) - margin_Y; //60% of screen available
+
+        for(let row = 0; row < nRows; row++) {
+            for(let col = 0; col < nCols; col++) {
+                let x_pos = (margin_X + brick_width) * col;
+                let y_pos = (margin_Y + brick_height) * row;
+
+                this.brickArray.push( new rectangle([margin_X + x_pos, margin_Y + y_pos], [brick_width, brick_height]));
+            }
+        }
+    }
+
+    brickArray = [];
+
 }
--- Project5/game.js
+++ Project5/game.js
@@ -1,38 +1,66 @@
+//@ts-check
 'use stric';
-import {rectangle, ball, screen} from "./classes.js";
+import { rectangle, ball, bricks, screen } from "./classes.js";
 
 let movement = 0;
-let speed = 20;
+let default_speed = 15;
+let speed = default_speed;
+let dV = 3;
+let maxV = 40;
+let score = 0;
 let barra;
 let gameScreen;
 let gameBall;
+let gameOver = true;
+let lifes = 3;
+let bricksArray;
 
 
 function initGame() {
     // create elements
     gameScreen = new screen(document.getElementById("game-window"));
     console.log(gameScreen);
+    bricksArray = new bricks(gameScreen, 5, 6);
     barra = new rectangle([100, 100], [200, 30]);
-    barra.setCenter([500, 450]);
     gameBall = new ball([500, 400]);
+
+    lifes = 3;
+    score = 0;
+    initUserPosition();
+}
+
+function initUserPosition() {
+    barra.setCenter([500, 450]);
+    gameBall.setPosition([500, 400]);
+    gameBall.speed = [5, -5];
 }
 
 function gameLoop() {
-    
-    //update
-    updateBarra(gameScreen);
-    updateBall(gameScreen);
 
-    // draw output   
-    draw();
+    if (!gameOver) {
+
+        console.log(`score ${score}`);
+
+        //update
+        updateBarra(gameScreen);
+        updateBall(gameScreen);
+
+        // draw output   
+        drawGame();
+    }
+    else {
+        drawGame();
+        drawMenu();
+    }
+    window.requestAnimationFrame(gameLoop);
 }
 
 function updateBarra(screen) {
-    if(movement == 1 && (barra.x + barra.width + speed) < screen.width) {
+    if (movement == 1 && (barra.x + barra.width + speed) < screen.width) {
         barra.x += speed;
         movement = 0;
     }
-    else if(movement == -1 && (barra.x - speed) > 0) {
+    else if (movement == -1 && (barra.x - speed) > 0) {
         barra.x -= speed;
         movement = 0;
     }
@@ -42,70 +70,170 @@ function updateBall(screen) {
     let new_X = gameBall.x + gameBall.speed[0];
     let new_Y = gameBall.y + gameBall.speed[1];
 
-    if(new_X > screen.width || new_X < 0) {
-        gameBall.speed[0] = -gameBall.speed[0]; 
+    if (new_X > screen.width || new_X < 0) {
+        gameBall.speed[0] = -gameBall.speed[0];
     }
 
-    if(new_Y > screen.height || new_Y < 0) {
-        gameBall.speed[1] = -gameBall.speed[1]; 
+    if (new_Y < 0) {
+        gameBall.speed[1] = -gameBall.speed[1];
+    }
+    else if((new_Y + gameBall.radius) > screen.height) {
+        lifes -= 1;
 
+        console.log(lifes);
+        
+        initUserPosition();
+        
+        if(lifes==0) {
+            gameOver = true;
+            initGame();
+        }
+
+        return;
     }
 
-    console.log(barra.y, new_Y);
-    if(new_Y > (barra.y) &&  new_Y < (barra.y + barra.heigth) && 
-        (new_X > barra.x) && (new_X < barra.x+barra.width)) 
-    {
-        gameBall.speed[1] = -gameBall.speed[1]; 
+
+    if (new_Y > (barra.y) && new_Y < (barra.y + barra.height) &&
+        (new_X > barra.x) && (new_X < barra.x + barra.width)) {
+        gameBall.speed[1] = -gameBall.speed[1];
     }
 
     gameBall.x = new_X;
     gameBall.y = new_Y;
+
+
+    // Brick collision detection
+    for (const brick of bricksArray.brickArray) {
+        if (new_Y > (brick.y) && new_Y < (brick.y + brick.height) &&
+            (new_X > brick.x) && (new_X < brick.x + brick.width)) {
+            const index = bricksArray.brickArray.indexOf(brick);
+            bricksArray.brickArray.splice(index, 1);
+            gameBall.speed[1] = -gameBall.speed[1];
+            score += 10;
+        }
+    }
+
+    //If all bricks are destroyed:
+    if(bricksArray.brickArray.length < 1 || bricksArray.brickArray == undefined){
+        gameOver = true;
+        initGame();
+    }
 }
 
-function draw() {
-    let canvas = document.getElementById("game-window");
-    let ctx = canvas.getContext('2d');
-
+function drawGame() {
     //Clean screen
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    gameScreen.ctx.clearRect(0, 0, gameScreen.width, gameScreen.height);
 
     //Draw bar
-    drawElement(ctx, barra);
-    drawBall(ctx, gameBall);
-    ctx.stroke();
+    drawElement(gameScreen.ctx, barra);
+    drawBall(gameScreen.ctx, gameBall);
+    
+    for (const brick of bricksArray.brickArray) {
+        drawElement(gameScreen.ctx, brick);
+    }
+    
+    drawGUI();
+    gameScreen.ctx.stroke();
 
 }
 
 function drawBall(ctx, ball) {
     ctx.beginPath();
-    ctx.arc(ball.x, ball.y , ball.radius, 0, 2*Math.PI);
+    ctx.arc(ball.x, ball.y, ball.radius, 0, 2 * Math.PI);
     ctx.fill();
 }
 
 function drawElement(ctx, rect) {
-    ctx.fillRect(rect.x, rect.y , rect.width, rect.heigth);
+    ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
+}
+
+
+function drawGUI() {
+    gameScreen.ctx.fillStyle="rgba(255, 255, 255, 0.75)";;
+    let textBackground = new rectangle([10, 10], [100, 30]);
+    drawElement(gameScreen.ctx, textBackground);
+    gameScreen.ctx.fillStyle="black";
+    gameScreen.ctx.font = "20px Arial";
+    gameScreen.ctx.fillText(`Score: ${score}`, 50, 25);    
+
+    gameScreen.ctx.stroke();
+}
+
+
+function drawMenu() {
+
+    let boxMargin = 100;
+    let textBackground = new rectangle([boxMargin, boxMargin], 
+        [gameScreen.width-boxMargin*2, gameScreen.height-boxMargin*2]);
+        
+        
+    drawElement(gameScreen.ctx, textBackground);
+        
+        
+    gameScreen.ctx.font = "40px Arial";
+    gameScreen.ctx.textAlign = "center";
+    gameScreen.ctx.fillStyle="white";
+    gameScreen.ctx.fillText("Break Out like Game", gameScreen.width/2, gameScreen.height/2);  
+    
+    gameScreen.ctx.font = "20px Arial";
+    gameScreen.ctx.fillText("Press Enter to start.", gameScreen.width/2, gameScreen.height/2+60);    
+    gameScreen.ctx.fillStyle="black";
+
+
 }
 
 // handle events
-function getInput(event) {
-    if(event.key == "ArrowLeft"){
-        movement =  -1;
+function getInputKeyDown(event) {
+
+    console.log(speed);
+
+    if (event.key == "ArrowLeft") {
+        movement = -1;
+        speed = Math.min(speed+dV, maxV);
     }
-    else if(event.key == "ArrowRight"){
-        movement =  +1;
+    else if (event.key == "ArrowRight") {
+        movement = +1;
+        speed = Math.min(speed+dV, maxV);
     }
     else {
         movement = 0;
     }
 
+    if (event.key == "Enter" && gameOver) {
+        gameOver = false;
+    }
 }
 
 
+function getInputKeyUp(event) {
+    if (event.key == "ArrowLeft") {
+        movement = -1;
+        speed = default_speed;
+    }
+    else if (event.key == "ArrowRight") {
+        movement = +1;
+        speed = default_speed;
+    }
+    else {
+        movement = 0;
+    }
+}
+
 document.addEventListener("keydown", event => {
     console.log(event.key);
-    getInput(event);
+    getInputKeyDown(event);
+});
+
+document.addEventListener("keyup", event => {
+    console.log(event.key);
+    getInputKeyUp(event);
 });
 
 
+
+
 initGame();
-setInterval(gameLoop, 50);
+
+window.requestAnimationFrame(gameLoop);
+
+// Try: https://newfivefour.com/javascript-canvas-smoothly-move-a-player.html
